1ccf5f30f653037b5e5d7c669ca3d1a11e33a985
Emad Akhras <emadakhrasidleb@gmail.com>
2024-12-01 15:28:17 +0800

    fish: Add clj vim iced to path

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,6 @@ end
 # Setting PATH for Python 3.<x>
 # set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
 set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$PATH"
+
+# Setting PATH for vim-iced
+set -x PATH "~/.config/nvim/autoload/vim-iced/bin" "$PATH"
