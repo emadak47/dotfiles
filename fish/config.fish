9b035c736106c8a76410784d04f79ef13f13947b
Emad Akhras <emadakhrasidleb@gmail.com>
2024-12-01 15:27:27 +0800

    fish: Add python to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Setting PATH for Python 3.<x>
+# set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$PATH"
