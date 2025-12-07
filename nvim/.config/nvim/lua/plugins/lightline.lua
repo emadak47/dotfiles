9f48225fd7c90a475e7e65abdf783596f29c93cb
Emad Akhras <emadakhrasidleb@gmail.com>
2025-12-07 17:54:13 +0800

    Buffer bar at the top
    barbar.nvim has been my default choice because it works with lightline
    after disabling lightline's tabline. bufferline.nvim wouldn't

--- nvim/.config/nvim/lua/plugins/lightline.lua
+++ nvim/.config/nvim/lua/plugins/lightline.lua
@@ -7,6 +7,11 @@ return {
 			-- don't show vim mode in command line
 			vim.o.showmode = false
 			vim.g.lightline = {
+				-- https://github.com/romgrk/barbar.nvim?tab=readme-ov-file#known-issues
+				enable = {
+					statusline = 1,
+					tabline = 0
+				},
 				active = {
 					left = {
 						{ 'mode', 'paste' },
