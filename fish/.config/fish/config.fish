9120ae1e6400db76bb52233d1fa4fcfe28767427
Emad Akhras <emadakhrasidleb@gmail.com>
2026-09-01 10:19:23 +0800

    use fd with fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -65,6 +65,10 @@ function gtree
     end
 end
 
+set -gx FZF_DEFAULT_COMMAND "fd --hidden --follow --strip-cwd-prefix --exclude .git"
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+# set -gx FZF_ALT_C_COMMAND "fd --type=d --hidden --follow --strip-cwd-prefix --exclude .git"
+
 fzf --fish | source
 zoxide init fish | source
 
